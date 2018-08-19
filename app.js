2a742944767dbbcdfc9c3e65ced7ced4e0c911b8
Clayton Cook <claytoncook@Claytons-MacBook-Pro.local>
2018-08-19 04:44:14 -0400

    📜 Added Simple UI and Story Container

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log("app.js intact");
+
+//elements
+const $game = document.querySelector("#game");
+const $buttons = document.querySelector("#buttons");
+const $story = document.querySelector("#story-text");
+
+//event listeners
+let storyPart = 0;
+$buttons.addEventListener("click", function (event) {
+    if (event.target !== event.currentTarget) {
+        let target = event.target;
+
+        if (target.innerHTML === "Walk") {
+            //show correct part of story
+            $story.innerHTML = story[storyPart][0];
+            //apply background color
+            $game.style.backgroundColor = story[storyPart][1];
+            $story.style.opacity = 1;
+            storyPart++;
+        }
+        console.log(target.innerHTML);
+    }
+    event.stopPropagation();
+}, false);
---
+++ data.js
@@ -0,0 +1,21 @@
+console.log("data.js intact");
+
+const data = {
+    enemies: [
+        {name: "Zombie", health: 10, attack: 10},
+        {name: "Skeleton", health: 10, attack: 10}
+    ]
+}
+
+//blank story: ["", "rgb(219, 219, 219)"]
+const story = [
+    ["You take a few steps forward", "rgb(219, 219, 219)", "sounds/ambience1.mp3"],
+    ["The whiteness begins to fade away", "rgb(219, 255, 219)", ""],
+    ["You take a few steps forward", "rgb(210, 255, 210)", ""],
+    ["The brightness settles into a pastel of green", "rgb(200, 255, 200)", ""],
+    ["You feel a warm wetness in the air", "rgb(210, 255, 210)", ""],
+    ["You push forward", "rgb(190, 255, 190)", ""],
+    ["You hear birds chirping in the distance", "rgb(175, 255, 175)", "sounds/birds1.mp3"],
+    ["Your vision clears", "rgb(160, 255, 160)", ""],
+    ["Trees surround you as you walk seeminglessly into a green forest", "rgb(120, 255, 120)", ""]
+]
